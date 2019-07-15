5085d590dd04fcc801e037437762d7bce4d3d7ff
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-15 20:06:54 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,9 @@
 # Build your say_hello method here
 def say_hello(name)
-  puts "Hello Kent Beck!"
+  puts "Hello, #{name}!"
 end
 
 
+def greeting(name = "Ruby programmer")
+  puts "Hello, #{name}"
+end
